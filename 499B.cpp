fd69c34563406820e3929e7fa9992c2e5f1f4583
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2022-01-05 18:58:15 +0600

    codeforces

--- 499B.cpp
+++ 499B.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+using namespace std;
+int main(){
+    int n,m;
+    map<string, string> map1;
+    cin>>n>>m;
+    while(m--){
+        string a,b;
+        cin>>a>>b;
+        map1[a] = b;
+    }
 
+    while(n--){
+        string s;
+        cin>>s;
+        string x,y;
+        x = s;
+        y = map1[s];
+        if(x.size()>y.size()){
+            cout<<y<<" ";
+        }else{
+            cout<<x<<" ";
+        }
+    }
+    return 0;
+}
