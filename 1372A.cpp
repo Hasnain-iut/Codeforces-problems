42aca7a358d53d5e58cf7e6cff8f9521385b7651
Hasnain-iut <94733956+Hasnain-iut@users.noreply.github.com>
2022-01-07 15:19:17 +0600

    codeforces

---
+++ 1372A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--){
+
+        int n;
+        cin>>n;
+        int x=n;
+        for(int i=0;i<n;i++){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
